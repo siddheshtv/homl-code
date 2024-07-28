e49f079a42b2844a98e805d16f13be14e011bc4e
siddheshtv <heykulthe@gmail.com>
2024-07-28 21:27:09 +0530

    add knn to chp1

--- 1/lin-mod-sklearn.py
+++ 1/lin-mod-sklearn.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 
-data_root = "https://github.com/ageron/data/raw/main"
+data_root = "https://github.com/ageron/data/raw/main/"
 lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
 X = lifesat[["GDP per capita (USD)"]].values
 y = lifesat[["Life satisfaction"]].values
@@ -13,10 +14,14 @@ lifesat.plot(kind="scatter",
              y="Life satisfaction",
              grid=True)
 plt.axis([23_500, 62_500, 4, 9])
+plt.savefig(fname="gdp_vs_life_sat.png", format="png")
 plt.show()
 
 model = LinearRegression()
+kmodel = KNeighborsRegressor()
 
 model.fit(X,y)
+kmodel.fit(X,y)
 X_new = [[37_655.2]]
 print(model.predict(X_new))
+print(kmodel.predict(X_new))
